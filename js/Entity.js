faa616fc14805ef1823cde2f11fd865b57a209e4
b4lee <iqbalanggoro48@gmail.com>
2020-03-27 17:47:26 +0700

    Entity moving to gm

--- js/Entity.js
+++
@@ -1,9 +0,0 @@
-const Entity = {
-    init: function (heroName, health, dodge, attack, armor) {
-        this.heroName = heroName;
-        this.health = health;
-        this.dodge = dodge;
-        this.attack = attack;
-        this.armor = armor;
-    }
-}
