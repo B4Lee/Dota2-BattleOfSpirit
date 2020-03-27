5a2fd558657e4fabadfecfbacd3d7bd986b7b6e8
b4lee <iqbalanggoro48@gmail.com>
2020-03-27 18:01:50 +0700

    Create Entity.js

---
+++ js/Entity.js
@@ -0,0 +1,10 @@
+
+const Entity = {
+    init: function (heroName, health, dodge, attack, armor) {
+        this.heroName = heroName;
+        this.health = health;
+        this.dodge = dodge;
+        this.attack = attack;
+        this.armor = armor;
+    }
+}
